d48b9db6b3abe0b129cf857821b1828d37f04439
MateusMSoares <74261014+MateusMSoares@users.noreply.github.com>
2023-09-29 15:30:02 -0300

    Adicionado comentários para ajudar na aprendizagem

--- Matematica_Discreta/Matriz.py
+++ Matematica_Discreta/Matriz.py
@@ -1,24 +1,34 @@
 #Criando matriz
 
 def criar_matriz_relacao(conjunto, relacoes):
-    matriz = [[''] + conjunto]  # primeira linha com os elementos do conjunto
+    matriz = [['='] + conjunto]  # rotular as colunas da matriz
     for elemento in conjunto:
-        linha = [elemento] + [0] * len(conjunto)
+        linha = [elemento] + [0] * len(conjunto) # rotular as linhas e adicionar zeros conforme o tamanho do conjunto
         matriz.append(linha)
-
-    # preenche a matriz com 1 onde a relação existe
+    print(f"\n 1) Como funciona? Primeira é criado uma lista de listas com os elementos do conjunto adicionando valor zero: \n{matriz}\n")
+    print(f"Pense agora que:")
+    # Preenche a matriz com 1 onde a relação existe
     for relacao in relacoes:
-        elemento1, elemento2 = relacao
-        indice1 = conjunto.index(elemento1) + 1
-        indice2 = conjunto.index(elemento2) + 1
-        matriz[indice1][indice2] = 1
+        elemento1, elemento2 = relacao # pega o elemento de cada relação
+        print(f" \nPara a relação: ({elemento1}, {elemento2})")
+        indice1 = conjunto.index(elemento1) + 1 # pega o índice do primeiro elemento
+        indice2 = conjunto.index(elemento2) + 1 # pega o índice do segundo elemento
+        print(f"Primeiro, verificamos as posições na matriz: [{indice1}][{indice2}]")
+        print(f'Se ambos elementos estão presentes na relação, atribuímos o valor 1 à posição [{indice1}][{indice2}]')
+        matriz[indice1][indice2] = 1 #linha e coluna da matriz
+        print(f"Assim, a matriz na linha {indice1} e coluna {indice2} se torna {matriz[indice1]}")
+        print(f"Assimilando o valor {matriz[indice1][indice2]} na posição [{indice1}][{indice2}]")
+
+
+    print('\n')
 
     return matriz
 
 conjunto = ['A', 'B', 'C', 'D']
-relacoes = [('A', 'B'), ('A', 'C'), ('B', 'C')]
+relacoes = [('A', 'A'), ('B', 'B'), ('C','C'),('D', 'D')]
+print("Criando matriz a partir de uma relação de conjuntos: \n")
+print(f"Dado o conjunto: {conjunto} e a relação {relacoes}. \n A sua matriz:")
 
 matriz_relacao = criar_matriz_relacao(conjunto, relacoes)
-
 for linha in matriz_relacao:
     print(' '.join(map(str, linha)))
